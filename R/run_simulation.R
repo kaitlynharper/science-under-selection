dc7dff75ec56323e2696f6bd221111d1eb3df6c6
Kati Harper <kaitlyn.harper@psy.lmu.de>
2025-09-09 11:26:50 +0200

    Add code to source model files, run model, and track performance

--- R/run_simulation.R
+++ R/run_simulation.R
@@ -1,1 +1,29 @@
-# Placeholder for running the simulations of academiABM2
+##############################################################################
+# Run simulation
+#
+# Description: This file sources the necessary files and provides code to run
+# academiABM2 simulations
+##############################################################################
+
+# Source model files
+source("R/functions.R")
+source("R/model.R")
+
+# Load required packages
+library(pryr) # For memory usage tracking
+library(profvis) # For performance profiling
+
+# Run simulation and track memory and runtime
+profvis_profile <- profvis({
+  results <<- run_simulation(
+    n_agents = 10000,
+    n_timesteps = 100,
+    papers_per_agent_per_timestep = 2
+  )
+})
+
+# Memory usage of results
+cat("=== MEMORY USAGE ===\n")
+cat("Papers matrix:", object_size(results), "bytes\n")
+
+print(profvis_profile)
