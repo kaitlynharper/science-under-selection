ce4aefbb83de2e3c771e0de652908fb155fe98f6
Kati Harper <kaitlyn.harper@psy.lmu.de>
2025-09-09 13:21:07 +0200

    Add a few basic tests

--- tests/testthat/test_placeholder.R
+++ tests/testthat/test_placeholder.R
@@ -1,5 +1,13 @@
-# Placeholder file for testthat tests
+# Tests
 
-test_that("Placeholder test", {
+test_that("the test framework is working", {
   expect_true(TRUE) #should always pass
+})
+
+test_that("there are no duplicate paper IDs", {
+  expect_equal(length(unique(results$papers[, 1])), nrow(results$papers))
+})
+
+test_that("the number of papers is correct", {
+  expect_equal(nrow(results$papers), n_agents * n_timesteps * papers_per_agent_per_timestep)
 })
