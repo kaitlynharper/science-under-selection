6d9eae5ba876301951456211594b152b137e15f4
Kati Harper <kaitlyn.harper@psy.lmu.de>
2025-10-27 08:20:40 +0100

    Remove nonworking tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,11 +7,3 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-
-# Source your R files to make functions available for testing
-source("/Users/kaitlynharper/Documents/Github/academiABM2/R/functions.R")
-source("/Users/kaitlynharper/Documents/Github/academiABM2/R/model.R")
-source("/Users/kaitlynharper/Documents/Github/academiABM2/R/run_simulation.R")
-
-# Run tests in the testthat directory
-test_dir("/Users/kaitlynharper/Documents/Github/academiABM2/tests/testthat")
