736cefc198393cc7fae3ac4e1d68c7fcd7bc946c
Kati Harper <kaitlyn.harper@psy.lmu.de>
2025-11-12 10:27:14 +0100

    Add columns for posterior distribution and research stage to effects matrix

--- R/functions/Initialize_effects_matrix.R
+++ R/functions/Initialize_effects_matrix.R
@@ -9,7 +9,7 @@ initialize_effects_matrix <- function(
   uninformed_prior_variance
 ) {
   # Initialize matrix of effects
-  effects <- matrix(0, nrow = n_effects, ncol = 6)
+  effects <- matrix(0, nrow = n_effects, ncol = 9)
   # Set column names
   colnames(effects) <- c(
     "effect_id",
@@ -17,8 +17,19 @@ initialize_effects_matrix <- function(
     "true_effect_size",
     "true_effect_variance",
     "prior_effect_size",
-    "prior_effect_variance"
+    "prior_effect_variance",
+    "posterior_effect_size",
+    "posterior_effect_variance",
+    "research_stage"
   )
+
+# I think I also need replication counter on effects matrix 
+# (so I can enter uninformed, original, and replication updates)
+# And also a column for the study id when the update happens
+# what if I make a column for each type of update? 
+# original_study_ID, original_posterior_mean and variance, replication_study_ID, replication_posterior_mean and variance
+# and get rid of the uninformed prior columns because they will be a constant
+
   # Generate ids
   effect_ids <- 1:n_effects
   # Set timestep
@@ -36,6 +47,11 @@ initialize_effects_matrix <- function(
   prior_effect_sizes <- rep(uninformed_prior_mean, n_effects)
   # Set uninformative prior variance
   prior_effect_variances <- rep(uninformed_prior_variance, n_effects)
+  # Initialize posterior (starts same as uninformed prior)
+  posterior_effect_sizes <- rep(uninformed_prior_mean, n_effects)
+  posterior_effect_variances <- rep(uninformed_prior_variance, n_effects)
+  # Initialize research_stage (0 = no studies done)
+  research_stages <- rep(0, n_effects)
 
   # Fill effects matrix with these values
   effects[
@@ -47,7 +63,10 @@ initialize_effects_matrix <- function(
       true_effect_sizes,
       true_effect_variances,
       prior_effect_sizes,
-      prior_effect_variances
+      prior_effect_variances,
+      posterior_effect_sizes,
+      posterior_effect_variances,
+      research_stages
     )
   return(effects)
 }
