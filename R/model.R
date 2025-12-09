4c8220c339e16b2086fa5137560246c678a1da37
Kati Harper <kaitlyn.harper@psy.lmu.de>
2025-12-09 12:46:53 +0100

    Generate initial agents with replication probability values of 0 or 1

--- R/model.R
+++ R/model.R
@@ -8,13 +8,13 @@
 run_simulation <- function(params) {
   # Create simulation environment
   sim_env <- new.env()
-  
+
   # TODO consider keeping the params bundled for easy export
   # Store parameters in environment
   for (param_name in names(params)) {
     sim_env[[param_name]] <- params[[param_name]]
   }
-  
+
   #### Initialize model ####
 
   # Initialize effects matrix
@@ -35,26 +35,27 @@ run_simulation <- function(params) {
     n_agents = sim_env$n_agents,
     timestep_active = 0,
     #replication_probabilities = runif(sim_env$n_agents, 0, 1),
-    replication_probabilities = sample(c(0,1), sim_env$n_agents, replace = T),
+    replication_probabilities = rbinom(sim_env$n_agents, 1, 0.5),
     target_powers = runif(sim_env$n_agents, 0, 1),
     timestep_next_papers = rep(0, sim_env$n_agents)
   )
 
   #### Timestep loop ####
   for (timestep in 0:sim_env$n_timesteps) {
-    
     # Update timestep tracker in the environment
     # (R doesn't like to use an environment variable as a loop index variable)
     sim_env$timestep <- timestep
-    
+
     print(paste0("Timestep ", timestep))
-    
+
     # Run actual studies
     run_studies(sim_env)
 
     # Career turnover phase (skip first career step)
-    if (sim_env$timestep %% sim_env$n_timesteps_per_career_step == 0 &&
-        sim_env$timestep > 0) {
+    if (
+      sim_env$timestep %% sim_env$n_timesteps_per_career_step == 0 &&
+        sim_env$timestep > 0
+    ) {
       career_turnover(sim_env)
     }
   }
