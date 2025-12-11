168e441740fb4383e8f79cfa31ed726a7f7856b1
Kati Harper <kaitlyn.harper@psy.lmu.de>
2025-12-11 11:02:54 +0100

    Update bulk script to use "initial_selection_condition"

--- R/run_conditions.R
+++ R/run_conditions.R
@@ -7,19 +7,19 @@
 library(here)
 
 # Set lists of condition parameters
-selection_condition_list <- c(0, 0, 1, 1, 1)
+initial_selection_condition_list <- c(0, 0, 1, 1, 1)
 switch_conditions_at_list <- c(NA, NA, NA, NA, 200)
 publication_bias_list <- c(0, 1, 0, 1, 1)
-n_timesteps_list <- c(200, 200, 200, 200, 400)
+n_timesteps_list <- c(350, 350, 350, 350, 700)
 
 # Loop through each condition
-for (i in 1:length(selection_condition_list)) {
-  selection_condition <- selection_condition_list[i]
+for (i in 1:length(initial_selection_condition_list)) {
+  initial_selection_condition <- initial_selection_condition_list[i]
   switch_conditions_at <- switch_conditions_at_list[i]
   publication_bias <- publication_bias_list[i]
   n_timesteps <- n_timesteps_list[i]
   source(here("R", "run_simulation.R"))
   source(here("R", "playing_around", "analysis_Felix.R"))
   # Save the object "patchwork" with the filename being a string of the four parameters above and their values
-  ggsave(here("output", paste0("selection", selection_condition, "_switch", switch_conditions_at, "_pub", publication_bias, "_t", n_timesteps, ".png")), patchwork, width = 30, height = 10)
+  ggsave(here("output", paste0("selection", initial_selection_condition, "_switch", switch_conditions_at, "_pub", publication_bias, "_t", n_timesteps, ".png")), patchwork_withtitle, width = 30, height = 10)
 }
