f75befe1910643b3eac070b469735abba0a7041a
Felix.S <felix@nicebread.de>
2025-12-11 10:07:39 +0100

    Refactor PB plot

---
+++ R/functions/PB_plot.R
@@ -0,0 +1,57 @@
+#' Visualize the logistic functions for publication bias
+
+#' @param sig_y_intercept minimum publication probability for p < .05 results
+#' @param sig_logistic_midpoint novelty midpoint for significant results
+#' @param sig_logistic_steepness steepness of logistic curve for significant results
+#' @param nonsig_logistic_midpoint novelty midpoint for non-significant results
+#' @param nonsig_logistic_steepness steepness of logistic curve for non-significant 
+
+# Medium PB:
+plot_PB(
+  sig_y_intercept = .2, sig_logistic_midpoint = 5, 
+  sig_logistic_steepness = 1, nonsig_logistic_midpoint = 1.5, 
+  nonsig_logistic_steepness = 3, subtitle="Medium PB", max_novelty = 10)
+
+# Strong PB:
+plot_PB(
+  sig_y_intercept = 0.8, sig_logistic_midpoint = 0.2, 
+  sig_logistic_steepness = 3, nonsig_logistic_midpoint = 3.5, 
+  nonsig_logistic_steepness = 3, subtitle="Strong PB")
+
+
+
+plot_PB <- function(sig_y_intercept, sig_logistic_midpoint, sig_logistic_steepness, nonsig_logistic_midpoint, nonsig_logistic_steepness, max_novelty = 5, subtitle=NA) {
+  
+  novelty_range <- seq(0, max_novelty, by = 0.01)
+
+  viz_data <- data.frame(
+    novelty = rep(novelty_range, 2),
+    publication_prob = c(
+      logistic_significant(novelty_range, sig_y_intercept, sig_logistic_midpoint, sig_logistic_steepness),
+      logistic_nonsignificant(novelty_range, nonsig_logistic_midpoint, nonsig_logistic_steepness)
+    ),
+    result_type = rep(
+      c("Significant", "Non-significant"),
+      each = length(novelty_range)
+    )
+  )
+
+  pub_prob <- ggplot(
+    viz_data,
+    aes(x = novelty, y = publication_prob, linetype = result_type)
+  ) +
+    geom_line(color = "black", linewidth = 1.5) +
+    theme_minimal() +
+    labs(
+      title = "Publication Probability by Novelty and Significance",
+      subtitle = subtitle,
+      x = "Novelty",
+      y = "Publication Probability",
+      linetype = "Result Type"
+    ) +
+    scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1)) +
+    scale_linetype_manual(values = c("dotted", "solid")) +
+    scale_x_continuous(limits = c(0, max_novelty))
+
+  pub_prob
+}
--- R/playing_around/analysis_Felix.R
+++ R/playing_around/analysis_Felix.R
@@ -95,26 +95,52 @@ study_type_count_per_effect <- studies |> group_by(effect_id) |>
 table(study_type_count_per_effect$n_original_studies)
 table(study_type_count_per_effect$n_replication_studies)
 
-S2 <- left_join(studies, effects, by="study_id")
+# Note: multiple original studies for one effect happen when the first original
+# study was not published, and another agent then chose it again.
+
+# as effects are stored multiple times, we reduce it to a single row per effect
+# (we just want the true effect size, not the evolution)
+effects_reduced <- effects |> select(effect_id, true_effect_size) |> distinct()
+S2 <- left_join(studies, effects_reduced, by="effect_id")
 
 S2$study_type_label <- factor(S2$study_type, levels=c(0, 1), labels=c("original", "replication"))
-S2$is_null <- factor(S2$true_effect_size == 0, levels=c(TRUE, FALSE), labels=c("null", "non-null"))
+S2$true_delta <- factor(S2$true_effect_size == 0, levels=c(TRUE, FALSE), labels=c("delta=0", "delta>0"))
+S2$publication_status_label <- factor(S2$publication_status, levels=c(0, 1), labels=c("unpublished", "published"))
+
+highest_original_novelty <- max(S2$novelty_contribution[S2$study_type == 0], na.rm=TRUE)
+S2 <- S2 |>
+  mutate(higher_repl_novelty = novelty_contribution > highest_original_novelty)
+
 
 ggplot(S2, aes(
   x=novelty_contribution, y=truth_contribution, 
-  color=study_type_label,
-  size=true_effect_size
+  color=study_type_label  
 )) +
-  geom_point(alpha=0.3) +
-  facet_wrap(is_null~study_type_label) +
+  geom_point(aes(shape = higher_repl_novelty), alpha=0.3) +
+  geom_smooth(method="lm", se=FALSE, color="grey60") +
+  facet_grid(true_delta~study_type_label~publication_status_label) +
   labs(title = TITLE, color="Study Type")
 
-print("% of replication studies with higher novelty score than the highest original study:")
-highest_original_novelty <- max(S2$novelty_contribution[S2$study_type == 0], na.rm=TRUE)
-S2 |>
-  filter(study_type == 1) |>
-  group_by(novelty_contribution > highest_original_novelty) |>
-  summarise(n = n())
-  
-  
-  sum(S2$study_type == 1 & S2$novelty_contribution > highest_original_novelty, na.rm=TRUE)
+print(paste0("% of replication studies with higher novelty score than the highest original study: ", (sum(S2$higher_repl_novelty & S2$study_type == 1, na.rm=TRUE) / sum(S2$study_type == 1, na.rm=TRUE) * 100) |> round(2), "%"))
+
+
+## Truth contribution of different study types
+## ----------------------------------------------------------------------
+## TODO: Dinstinguish replication of sig. originals vs. replications of non-sig. originals
+
+S2 |> 
+  group_by(type=study_type_label, true_delta, pub=publication_status_label) |>
+  summarise(
+    mean_estimate = mean(estimated_mean, na.rm=TRUE),
+    mean_abs_estimate = mean(abs(estimated_mean), na.rm=TRUE),   # take the absolute estimate so that positive and negative effects don't cancel out
+    mean_truth_contrib = mean(truth_contribution, na.rm=TRUE),
+    mean_novel_contrib = mean(novelty_contribution, na.rm=TRUE),    
+    k_studies = n()
+  ) |>
+  arrange(-mean_truth_contrib)
+
+
+
+# # show histogram of line 7: 
+# S2 |> filter(study_type == 1, true_effect_size==0, publication_status==1) |>
+#   pull("estimated_mean") |> hist(main="Published original studies of a true null", xlab="")
